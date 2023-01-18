0a1ac56a8b95397496e2199176752a9de2b99999
lingtheroof <122776634+lingtheroof@users.noreply.github.com>
2023-01-18 14:52:17 +0800

    Update HeaderMenuContent.jsx

--- src/components/common/header/HeaderMenuContent.jsx
+++ src/components/common/header/HeaderMenuContent.jsx
@@ -7,7 +7,7 @@ const HeaderMenuContent = ({ float = "" }) => {
   const home = [
     {
       id: 1,
-      name: "Home 1",
+      name: "Home 6",
       routerPath: "/",
     },
     { id: 2, name: "Home 2", routerPath: "/home-2" },
@@ -18,7 +18,7 @@ const HeaderMenuContent = ({ float = "" }) => {
     },
     { id: 4, name: "Home 4", routerPath: "/home-4" },
     { id: 5, name: "Home 5", routerPath: "/home-5" },
-    { id: 6, name: "Home 6", routerPath: "/home-6" },
+    { id: 6, name: "Home 1", routerPath: "/home-6" },
     { id: 7, name: "Home 7", routerPath: "/home-7" },
     { id: 8, name: "Home 8", routerPath: "/home-8" },
     { id: 9, name: "Home 9", routerPath: "/home-9" },
