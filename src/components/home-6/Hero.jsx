88019e32ccaf728f919ce9ab35157b2c46ace002
lingtheroof <122776634+lingtheroof@users.noreply.github.com>
2023-01-19 12:48:15 +0800

    a

--- src/components/home-6/Hero.jsx
+++ src/components/home-6/Hero.jsx
@@ -18,31 +18,6 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
- {/* End .container */}
-
- <div className="container ovh">
-        <div className="row">
-          <div className="col-lg-6 offset-lg-3">
-            <div className="main-title text-center mb40">
-              <h2>Latest For Sale</h2>
-              <p>Handpicked properties by our team.</p>
-            </div>
-          </div>
-          {/* End .col */}
-
-          <div className="col-lg-12">
-            <div className="feature_property_slider gutter-x15">
-              <FeaturedProperties />
-            </div>
-          </div>
-          {/* End .col */}
-        </div>
-        {/* End .row */}
-      </div>
-      {/* End .container .ovh */}
-
-
     </section>
   );
 };
--- src/components/home-6/index.jsx
+++ src/components/home-6/index.jsx
@@ -27,7 +27,54 @@ const index = () => {
       <Hero />
 
       {/* <!-- Feature Properties --> */}
+
+
+      <section className="latest-property">
+
+      <div className="container ovh">
+        <div className="row">
+          <div className="col-lg-6 offset-lg-3">
+            <div className="main-title text-center mb40">
+              <h2>Latest For Sale</h2>
+              <p>Handpicked properties by our team.</p>
+            </div>
+          </div>
+          {/* End .col */}
+
+          <div className="col-lg-12">
+            <div className="feature_property_slider gutter-x15">
+              <FeaturedProperties />
+            </div>
+          </div>
+          {/* End .col */}
+        </div>
+        {/* End .row */}
+      </div>
+
+        <div className="container ovh">
+          <div className="row">
+            <div className="col-lg-6 offset-lg-3">
+              <div className="main-title text-center mb40">
+                <h2>Latest For Rent</h2>
+                <p>Handpicked properties by our team.</p>
+              </div>
+            </div>
+            {/* End .col */}
+
+            <div className="col-lg-12">
+              <div className="feature_property_slider gutter-x15">
+                <FeaturedProperties />
+              </div>
+            </div>
+            {/* End .col */}
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container .ovh */}
+      </section>
+
       <section id="feature-property" className="feature-property-home6">
+        
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
